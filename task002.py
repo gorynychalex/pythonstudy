b77009276643c24b5d5d092d881a0c88f540ebb3
Alexey <gorynychalex@gmail.com>
2023-03-13 15:53:19 +0500

    add files

---
+++ task002.py
@@ -0,0 +1,7 @@
+print("Программа для вычислений.\n")
+##
+print("Введите 2 числа для сложения: ")
+a = int(input("a = "))
+b = int(input("b = "))
+c = a + b
+print(str(a) + " + " + str(b) + " = " + str(c))
